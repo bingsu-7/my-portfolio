022dccb7fca5399e0d018e11aa0321b4e06e4a92
김소영 <vkxqldtncprtm@naver.com>
2025-05-25 18:35:12 +0900

    Fix line endings for nice-select.css

--- js/Top.js
+++ js/Top.js
@@ -1,14 +1,3 @@
-// $(function() {
-//     $(".order").click(function(e) {
-//         e.stopPropagation();
-//         $(".order ul").css("display","block");
-//     });
-//     $(document).click(function() {
-//         $(".order ul").css("display","none");
-//     });
-
-// })
-// 셀렉트박스 플러그인
  $(document).ready(function() {
         $('select').niceSelect();
     });
